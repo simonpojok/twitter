1c5c23d99f97f6326f64c722dd35d6499980f950
simonpojok <simonpojok@gmail.com>
2022-09-21 15:36:51 +0300

    Initial Commit

---
+++ Twittor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twittor
+//
+//  Created by Simon Peter Ojok on 21/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
